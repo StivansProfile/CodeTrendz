30af8b7ab407edd0cfac30aae1514bf7888a4c5e
StevenSon12 <stivanstf55@gmail.com>
2023-11-14 10:58:11 +0000

    Web Scrapper class design

--- CodeTrendz/scripts/scrapper.py
+++ CodeTrendz/scripts/scrapper.py
@@ -1,4 +1,4 @@
-class Web_Scrapper:
+class Web_Scrapper:  # web scrapper design
     def __init__(self, platform_to_scrape, number_of_job_posts):
         self.platform_to_scrape = platform_to_scrape
         self.num_of_job_posts = number_of_job_posts
