93b868bb73baf3c81c97696f248d7f19b7983368
StevenSon12 <stivanstf55@gmail.com>
2023-11-14 13:08:56 +0000

    Figured out how to scrape multiple job posts Implementation to be done soon :)

--- CodeTrendz/scripts/scrapper.py
+++ CodeTrendz/scripts/scrapper.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-# ! Linked In only
+# ! Linked In only for now
+
+"""
+To scrape multiple pages:
+https://uk.linkedin.com/jobs/software-engineer-jobs?currentJobId=3758062458&position=1&pageNum=0
+
+collect the ulrs based on position
+- scrape the whole page and find the urls with position 1 to maybe 50
+- find a way to click on each url and for each url perform the scrapping function
+
+"""
 
 
 class Web_Scrapper:  # web scrapper design
@@ -9,8 +19,12 @@ class Web_Scrapper:  # web scrapper design
         self.platform_to_scrape = platform_to_scrape
         self.num_of_job_posts = number_of_job_posts
 
+        self.job_urls = []
         self.company_name = ""
         self.job_description = ""
+
+    def find_job_urls(self):
+        pass
 
     def scrape(self):
         response = requests.get(self.platform_to_scrape)
@@ -41,7 +55,7 @@ class Web_Scrapper:  # web scrapper design
 
 
 web_scrapper = Web_Scrapper(
-    "https://uk.linkedin.com/jobs/view/junior-frontend-developer-fully-remote-at-it-talent-solutions-3748049402?refId=GDeZd2Pdvnz2zSledLl2Pw%3D%3D&trackingId=CXzc%2FcrFTuBadYff4oxX9A%3D%3D&trk=public_jobs_topcard-title&original_referer=https%3A%2F%2Fuk.linkedin.com%2Fjobs%2Fsoftware-engineer-jobs%3FcurrentJobId%3D3748049402%26position%3D3%26pageNum%3D0",
+    "https://uk.linkedin.com/jobs/view/graduate-software-engineer-up-to-%C2%A375-000-%2B-bonus-%2B-package-at-hunter-bond-3762161377?refId=0DlIIe2tVFfIVv%2BFE%2FBVlQ%3D%3D&trackingId=NSOHV1jGfjqIHB%2BCKsFxgQ%3D%3D&trk=public_jobs_topcard-title",
     5,
 )
 web_scrapper.scrape()
