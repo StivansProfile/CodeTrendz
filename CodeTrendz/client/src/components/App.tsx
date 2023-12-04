16f521ca5b38c6f1d106bab1f3841ca84610694b
StevenSon12 <stivanstf55@gmail.com>
2023-12-04 22:38:58 +0000

    Started working on the client side

---
+++ CodeTrendz/client/src/components/App.tsx
@@ -0,0 +1,22 @@
+import "../styles/App.css"
+
+function App() {
+  return(
+    <div className="app-wrapper">
+
+      <nav>
+        <div className="nav-logo">
+          <h1>CodeTrendz</h1>
+        </div>
+        <div className="nav-links">
+          <a>About</a>
+          <a>Contact</a>
+          <a>Contribute</a>
+        </div>
+      </nav>
+    </div>
+  )
+ 
+}
+
+export default App
