e03d348e727856c8d33ab5c02b6e3fd58de252eb
StevenSon12 <stivanstf55@gmail.com>
2023-12-16 14:22:32 +0000

    Updating the list of technologies to look for

--- CodeTrendz/scripts/skills_analyser.py
+++ CodeTrendz/scripts/skills_analyser.py
@@ -2,22 +2,153 @@ import spacy, csv
 import pandas as pd
 from scrapper import Web_Scrapper
 
-nlp = spacy.load("en_core_web_sm")
 
-web_scrapper = Web_Scrapper(
-    "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?currentJobId=3744358098&keywords=software%20developer&originalSubdomain=uk&start=150",
-    5,
-)
-web_scrapper.scrape()
+def scrape_and_store_data():
+    nlp = spacy.load("en_core_web_sm")
 
-# Sample unstructured job posting text
-job_posting_texts = web_scrapper.job_description
+    web_scrapper = Web_Scrapper(
+        "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?currentJobId=3744358098&keywords=software%20developer&originalSubdomain=uk&start=150",
+        5,
+    )
+    web_scrapper.scrape()
 
-# Initialize lists to store extracted skills, programming languages, and education
-skills = []
-programming_languages = []
-education = []
+    # Sample unstructured job posting text
+    job_posting_texts = web_scrapper.job_description
 
+    # Initialize lists to store extracted skills, programming languages, and education
+    skills = []
+    programming_languages = []
+    education = []
+
+    languages_technologies = [
+        "Python",
+        "JavaScript",
+        "Java",
+        "C++",
+        "C#",
+        "PHP",
+        "Ruby",
+        "Swift",
+        "TypeScript",
+        "Kotlin",
+        "Go",
+        "Rust",
+        "Perl",
+        "Objective-C",
+        "Scala",
+        "HTML/CSS",
+        "SQL",
+        "Shell",
+        "Assembly",
+        "R",
+        "Matlab",
+        "Dart",
+        "Haskell",
+        "Groovy",
+        "Lua",
+        "VB.NET",
+        "VBA",
+        "COBOL",
+        "Fortran",
+        "Ada",
+        "Lisp",
+        "Scheme",
+        "Prolog",
+        "Smalltalk",
+        "Erlang",
+        "Clojure",
+        "F#",
+        "Verilog",
+        "VHDL",
+        "LabVIEW",
+        "React",
+        "Angular",
+        "Vue.js",
+        "Node.js",
+        "Express.js",
+        "Django",
+        "Flask",
+        "Ruby on Rails",
+        "Spring Framework",
+        "ASP.NET",
+        "Laravel",
+        "Symfony",
+        "TensorFlow",
+        "PyTorch",
+        "Keras",
+        "Spark",
+        "Hadoop",
+        "MongoDB",
+        "MySQL",
+        "PostgreSQL",
+        "Redis",
+        # ... Feel free to add more languages, frameworks, or technologies here
+    ]
+
+    found_languages = []
+
+    # Extraction of relevant information
+    for text in job_posting_texts:
+        doc = nlp(text)
+
+        # Define keywords related to skills, programming languages, and education
+        skill_keywords = ["skills", "abilities", "experience", "knowledge"]
+        language_keywords = ["programming languages", "coding languages"]
+        education_keywords = ["degree", "qualification", "education"]
+
+        # Extract information using spaCy
+        for sentence in doc.sents:
+            # Extract skills
+            if any(keyword in sentence.text.lower() for keyword in skill_keywords):
+                for token in sentence:
+                    if token.pos_ == "NOUN" or token.pos_ == "PROPN":
+                        skills.append(token.text)
+
+            # Extract programming languages
+            if any(keyword in sentence.text.lower() for keyword in language_keywords):
+                for token in sentence:
+                    if token.pos_ == "NOUN" or token.pos_ == "PROPN":
+                        programming_languages.append(token.text)
+
+            # Extract education requirements
+            if any(keyword in sentence.text.lower() for keyword in education_keywords):
+                for token in sentence:
+                    if token.pos_ == "NOUN" or token.pos_ == "PROPN":
+                        education.append(token.text)
+
+        # Remove duplicates and display the extracted information
+        skills = list(set(skills))
+        programming_languages = list(set(programming_languages))
+        education = list(set(education))
+
+        print("Extracted Skills:", skills)
+        print("Extracted Programming Languages:", programming_languages)
+        print("Extracted Education Requirements:", education)
+
+    file_path = "C:\\Users\\disco\\Desktop\\Programming\Portfolio\\CodeTrendzRepo\\CodeTrendz\\scripts\\data\\skills_data.csv"
+
+    # Saving the extracted information to a CSV file
+    with open(file_path, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+
+        # Write headers
+        writer.writerow(["Skills", "Programming Languages", "Education Requirements"])
+
+        if len(skills) != 0 or len(programming_languages) != 0 or len(education) != 0:
+            # Combine lists into rows to ensure each row has all data
+            max_length = max(len(skills), len(programming_languages), len(education))
+            for i in range(max_length):
+                skill = skills[i] if i < len(skills) else ""
+                lang = (
+                    programming_languages[i] if i < len(programming_languages) else ""
+                )
+                edu = education[i] if i < len(education) else ""
+                writer.writerow([skill, lang, edu])
+
+        print("Data saved to 'skills_data.csv'")
+
+
+file_path = "C:\\Users\\disco\\Desktop\\Programming\Portfolio\\CodeTrendzRepo\\CodeTrendz\\scripts\\data\\skills_data.csv"
 languages_technologies = [
     "Python",
     "JavaScript",
@@ -80,66 +211,41 @@ languages_technologies = [
     "MySQL",
     "PostgreSQL",
     "Redis",
-    # ... Feel free to add more languages, frameworks, or technologies here
+    "GitHub",
+    "HTML",
+    "CSS",
+    "HTML5",
+    "CSS3",
+    "jQuery",
+    "Git",
+    "TypeScript",
 ]
 
-found_languages = []
+df = pd.read_csv(file_path)
 
-# Extraction of relevant information
-for text in job_posting_texts:
-    doc = nlp(text)
-
-    # Define keywords related to skills, programming languages, and education
-    skill_keywords = ["skills", "abilities", "experience", "knowledge"]
-    language_keywords = ["programming languages", "coding languages"]
-    education_keywords = ["degree", "qualification", "education"]
-
-    # Extract information using spaCy
-    for sentence in doc.sents:
-        # Extract skills
-        if any(keyword in sentence.text.lower() for keyword in skill_keywords):
-            for token in sentence:
-                if token.pos_ == "NOUN" or token.pos_ == "PROPN":
-                    skills.append(token.text)
-
-        # Extract programming languages
-        if any(keyword in sentence.text.lower() for keyword in language_keywords):
-            for token in sentence:
-                if token.pos_ == "NOUN" or token.pos_ == "PROPN":
-                    programming_languages.append(token.text)
-
-        # Extract education requirements
-        if any(keyword in sentence.text.lower() for keyword in education_keywords):
-            for token in sentence:
-                if token.pos_ == "NOUN" or token.pos_ == "PROPN":
-                    education.append(token.text)
-
-    # Remove duplicates and display the extracted information
-    skills = list(set(skills))
-    programming_languages = list(set(programming_languages))
-    education = list(set(education))
-
-    print("Extracted Skills:", skills)
-    print("Extracted Programming Languages:", programming_languages)
-    print("Extracted Education Requirements:", education)
+# Initialize a dictionary to store occurrences of technologies
+tech_occurrences = {tech: 0 for tech in languages_technologies}
 
 
-file_path = "C:\\Users\\disco\\Desktop\\Programming\Portfolio\\CodeTrendzRepo\\CodeTrendz\\scripts\\data\\skills_data.csv"
+# Function to find and count occurrences of technologies in a string
+def find_technologies(text):
+    if isinstance(text, str):  # Check if the value is a string
+        for tech in languages_technologies:
+            if tech in text:
+                tech_occurrences[tech] += 1
 
-# Saving the extracted information to a CSV file
-with open(file_path, mode="w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
 
-    # Write headers
-    writer.writerow(["Skills", "Programming Languages", "Education Requirements"])
+# Apply the function to every cell in the dataframe
+df.applymap(find_technologies)
 
-    if len(skills) != 0 or len(programming_languages) != 0 or len(education) != 0:
-        # Combine lists into rows to ensure each row has all data
-        max_length = max(len(skills), len(programming_languages), len(education))
-        for i in range(max_length):
-            skill = skills[i] if i < len(skills) else ""
-            lang = programming_languages[i] if i < len(programming_languages) else ""
-            edu = education[i] if i < len(education) else ""
-            writer.writerow([skill, lang, edu])
+# Get the duplicates
+duplicates = {tech: count for tech, count in tech_occurrences.items() if count > 1}
 
-    print("Data saved to 'skills_data.csv'")
+# Print occurrences and duplicates
+print("Occurrences of technologies:")
+for tech, count in tech_occurrences.items():
+    if count > 0:
+        print(f"{tech}: {count} occurrences")
+
+print("\nDuplicates:")
+print(duplicates)
